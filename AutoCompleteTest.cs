9b078b0d1e4f1c37a4b758cba4455b30f5c1ff96
Tony Harverson <Tony.Harverson@buyagift.com>
2023-12-07 17:31:05 +0000

    Initial Example

---
+++ AutoCompleteTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AutoCompleteTest
+{
+    public class AutoCompleteTest
+    {
+        private readonly ILogger<AutoCompleteTest> _logger;
+
+        public AutoCompleteTest(ILogger<AutoCompleteTest> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(AutoCompleteTest))]
+        public void Run([ServiceBusTrigger("{your-queuename-here}", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message)
+        {
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+        }
+    }
+}
